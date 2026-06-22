ad057582281371e42d192b04877556c64cfc453f
MYB0614 <k01051230954@gmail.com>
2026-06-22 17:46:00 +0900

    Create 05-1-5.c
    문자를 정수로 변환하여 출력

---
+++ 05-1-5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+    int main(void){ 
+        char cng;
+        printf("정수로 바꿀 문자를 입력하세요.");
+        scanf("%c",&cng);
+        printf("%d",cng);
+        return 0;
+        
+        
+    }
